e8777195b93808d9ce8f289839b070a03e0ff5a8
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-05-31 21:13:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("btn");
+const body = document.querySelector("body")
+
+const showColors = () => {
+   const colors = ["red", "yellow", "blue", "pink", "purple", "cyan", "orange"];
+   const random = Math.floor(Math.random ()* colors.length);
+   body.style.backgroundColor = colors[random]
+
+}
+
+btn.addEventListener("click", showColors);
