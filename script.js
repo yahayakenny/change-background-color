9c9db2b2923c83067460bd23b0806ab3573e4d1d
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-05-31 21:22:15 +0100

    fixed some bugs

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const showColors = () => {
    const colors = ["red", "yellow", "blue", "pink", "purple", "cyan", "orange"];
    const random = Math.floor(Math.random ()* colors.length);
    body.style.backgroundColor = colors[random]
-
 }
 
 btn.addEventListener("click", showColors);
